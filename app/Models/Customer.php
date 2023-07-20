da965b785d37921282c7fd5c7956d16b0a15af8d
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2023-07-20 18:58:28 +0330

    get all active users

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Log;
 use Symfony\Component\Uid\Ulid;
 
@@ -23,13 +24,30 @@ class Customer extends Model
         'is_active' => true,
     ];
 
+    protected $appends = [
+        'full_url',
+    ];
+
     public static function store($data)
     {
-        Log::info('store', $data);
         return Customer::query()->create([
             'name' => $data['name'],
             'hours' => $data['hours'],
             'token' => Ulid::generate(),
         ]);
+    }
+
+    public static function getActives()
+    {
+        return Customer::query()->where('is_active', true)
+            ->where(function ($query) {
+                return $query->whereNull('valid_until')
+                    ->orWhere('valid_until', '<=', Carbon::now());
+            })->orderBy('created_at', 'DESC')
+            ->get();
+    }
+
+    public function getFullUrlAttribute() {
+        return rtrim(config('app.url'), '/') . '/tutorials/' . $this->token;
     }
 }
