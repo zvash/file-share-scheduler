2057e5afddebfaad032ab29d25a231fec0c16998
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2023-07-21 04:05:20 +0330

    tutorial migrations

---
+++ database/migrations/2023_07_20_170231_create_tutorials_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tutorials', function (Blueprint $table) {
+            $table->id();
+            $table->string('token');
+            $table->string('title');
+            $table->text('thumbnail_url')->nullable();
+            $table->text('download_url');
+            $table->string('size')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tutorials');
+    }
+};
