001bce574845c273b6420f1ca56519f80cad06c3
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2023-07-21 16:46:03 +0330

    added tutorials section to the responsive menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -80,6 +80,10 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('tutorials.index')" :active="request()->routeIs('tutorials.index')">
+                {{ __('Tutorials') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
